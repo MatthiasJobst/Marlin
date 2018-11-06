6f21eb1c9e7e40362c0289ecf85fd80ce81ae176
Matthias Jobst <matthias.jobst@gmx.net>
2018-11-06 13:30:05 +0100

    This version works on test prints.  It also allows to switch the case light on and off.
    There are still some quirks.
    
    Many thanks to the following links to help me get to here:
    http://www.geeetech.com/forum/viewtopic.php?f=20&t=17046&p=38551&hilit=mecreator#p38551
    http://www.geeetech.com/forum/viewtopic.php?f=20&t=17046&p=36197&hilit=mecreator#p36197

--- Marlin/boards.h
+++ Marlin/boards.h
@@ -104,7 +104,7 @@
 #define BOARD_SCOOVO_X9H        321   // abee Scoovo X9H
 #define BOARD_GT2560_REV_A      74    // Geeetech GT2560 Rev. A
 #define BOARD_GT2560_REV_A_PLUS 75    // Geeetech GT2560 Rev. A+ (with auto level probe)
-
+#define BOARD_GT2560_REV_B      76    // Geeetech GT2560 Rev. B as used in MeCreator 2
 //
 // ATmega1281, ATmega2561
 //
